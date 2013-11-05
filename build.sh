e54997f950d19553991420d5b00726930fca1690
Matt Thomas <matt@betweenbrain.com>
2013-11-04 22:33:25 -0500

    Initial attempt

---
+++ build.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+#
+#
+echo "Make sure we have the latest updates."
+echo "---------------------------------------------------------------"
+#
+aptitude update && aptitude -y safe-upgrade
+#
+echo "Setting the hostname."
+echo "---------------------------------------------------------------"
+#
+hostname vagrant-debian-squeeze
+#
+echo "Adding user vagrant"
+echo "---------------------------------------------------------------"
+# -------------------------------------------------------------------------
+# Script to add a user to Linux system
+# -------------------------------------------------------------------------
+# Copyright (c) 2007 nixCraft project <http://bash.cyberciti.biz/>
+# This script is licensed under GNU GPL version 2.0 or above
+# Comment/suggestion: <vivek at nixCraft DOT com>
+# -------------------------------------------------------------------------
+# See url for more info:
+# http://www.cyberciti.biz/tips/howto-write-shell-script-to-add-user.html
+# -------------------------------------------------------------------------
+if [ $(id -u) -eq 0 ]; then
+	egrep "^vagrant" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "vagrant exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' vagrant)
+		useradd -s /bin/bash -m -d /home/vagrant -U -p $pass vagrant
+		[ $? -eq 0 ] && echo "User vagrant has been added to system!" || echo "Failed to add user vagrant!"
+	fi
+else
+	echo "Only root may add a user to the system"
+	exit 2
+fi
+# -------------------------------------------------------------------------
+# End script to add a user to Linux system
+# -------------------------------------------------------------------------
+#
+#
+echo "Creating admin group and Vagrant user."
+echo "---------------------------------------------------------------"
+#
+groupadd admin
+#
+sudo usermod -G admin vagrant
+#
+echo "Add admin group to sudoers and make passwordless."
+echo "---------------------------------------------------------------"
+#
+echo "
+Defaults env_keep=\"SSH_AUTH_SOCK\"
+%admin ALL=NOPASSWD: ALL
+" >> /etc/sudoers
+#
+#
+echo "Install ruby and chef."
+echo "---------------------------------------------------------------"
+#
+#
+apt-get -y update
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
+cd /tmp
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
+tar -xvzf ruby-1.9.3-p125.tar.gz
+cd ruby-1.9.3-p125/
+./configure --prefix=/usr/local
+make
+make install
+gem install chef ruby-shadow --no-ri --no-rdoc
+#
+#
+echo "Install puppet."
+echo "---------------------------------------------------------------"
+#
+#
+sudo apt-get -y install puppet puppetmaster
+#
+#
+echo "Install openssh-server packages."
+echo "---------------------------------------------------------------"
+#
+#
+sudo apt-get -y install openssh-server
+#
+#
+echo "Install misc packages."
+echo "---------------------------------------------------------------"
+#
+#
+sudo apt-get -y install git-core
+#
+#
+echo "Install Apache2."
+echo "---------------------------------------------------------------"
+#
+#
+sudo apt-get install apache2
+#
+sudo a2enmod rewrite deflate
+#
+#
+echo "
+User vagrant
+Group vagrant
+" >> /etc/apache2/httpd.conf
+#
+#
+echo "Installing debconf utilities"
+echo "---------------------------------------------------------------"
+#
+aptitude install -y debconf-utils
+#
+#
+echo mysql-server mysql-server/root_password password | sudo debconf-set-selections
+echo mysql-server mysql-server/root_password_again password | sudo debconf-set-selections
+#
+sudo apt-get install mysql-server
+#
+#
+sudo apt-get install php5 php-pear php5-suhosin php5-cli php5-mysql php5-curl php5-memcache
+#
+#
+/etc/init.d/apache2 restart
+#
+#
+sudo mkdir /var/www/adminer
+#
+#
+wget http://www.adminer.org/latest.php -P /var/www/index.php -o index.php
+#
+#
+echo "Install vagrant's public keys."
+echo "---------------------------------------------------------------"
+#
+#
+sudo chown vagrant:vagrant /var/www
+find /var/www/ -type d -exec chmod 755 {} \;
+find /var/www/ -type f -exec chmod 644 {} \;
+#
+#
+echo "Install vagrant's public keys."
+echo "---------------------------------------------------------------"
+#
+#
+mkdir ~/.ssh/
+chmod 0755 ~/.ssh
+cd ~/.ssh
+wget http://github.com/mitchellh/vagrant/raw/master/keys/vagrant
+wget http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
+mv vagrant.pub authorized_keys
+chmod 0644 authorized_keys
+#
+#
+echo "Install virtual box guest additions."
+echo "---------------------------------------------------------------"
+#
+#
+sudo apt-get -y install linux-headers-$(uname -r) build-essential dkms
+#
+sudo apt-get clean
+#
+sudo reboot
+#
+#
