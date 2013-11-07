a52d0738ed4d5e9060487d74efbc28cafebcb336
Matt Thomas <matt@betweenbrain.com>
2013-11-07 09:01:33 -0500

    Basic AMP stack

--- build.sh
+++ build.sh
@@ -6,58 +6,6 @@ echo "---------------------------------------------------------------"
 #
 aptitude update && aptitude -y safe-upgrade
 #
-echo "Setting the hostname."
-echo "---------------------------------------------------------------"
-#
-echo "squeeze" > /etc/hostname
-hostname -F /etc/hostname
-#
-#
-echo "Creating admin group and Vagrant user."
-echo "---------------------------------------------------------------"
-#
-groupadd admin
-#
-usermod -G admin vagrant
-#
-echo "Add admin group to sudoers and make passwordless."
-echo "---------------------------------------------------------------"
-#
-echo "
-Defaults env_keep=\"SSH_AUTH_SOCK\"
-%admin ALL=NOPASSWD: ALL
-" >> /etc/sudoers
-#
-#
-echo "Install ruby and chef."
-echo "---------------------------------------------------------------"
-#
-#
-apt-get -q -y update
-apt-get -q -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
-cd /tmp
-wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
-tar -xvzf ruby-1.9.3-p125.tar.gz
-cd ruby-1.9.3-p125/
-./configure --prefix=/usr/local
-make
-make install
-gem install chef ruby-shadow --no-ri --no-rdoc
-#
-#
-echo "Install puppet."
-echo "---------------------------------------------------------------"
-#
-#
-apt-get -q -y install puppet puppetmaster
-#
-#
-echo "Install openssh-server packages."
-echo "---------------------------------------------------------------"
-#
-#
-apt-get -q -y install openssh-server
-#
 #
 echo "Install misc packages."
 echo "---------------------------------------------------------------"
@@ -100,38 +48,13 @@ mkdir /var/www/adminer
 wget http://www.adminer.org/latest.php -P /var/www/index.php -o index.php
 #
 #
-echo "Install vagrant's public keys."
-echo "---------------------------------------------------------------"
+echo "Make vagrant owner of /var/www"
+echo "--------------------------------------------------------------"
 #
 #
 chown vagrant:vagrant /var/www
-find /var/www/ -type d -exec chmod 755 {} \;
-find /var/www/ -type f -exec chmod 644 {} \;
 #
 #
-echo "Install vagrant's public keys."
-echo "---------------------------------------------------------------"
-#
-#
-mkdir ~/.ssh/
-chmod 0755 ~/.ssh
-cd ~/.ssh
-wget http://github.com/mitchellh/vagrant/raw/master/keys/vagrant
-wget http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
-mv vagrant.pub authorized_keys
-chmod 0644 authorized_keys
-#
-#
-echo "Install virtual box guest additions."
-echo "---------------------------------------------------------------"
-#
-#
-apt-get -q -y install linux-headers-$(uname -r) build-essential dkms
-#
-apt-get clean
-#
-#
-echo
 echo "One final hurrah"
 echo "--------------------------------------------------------------"
 #
